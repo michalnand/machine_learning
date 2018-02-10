3fbb76414f8ab0dfc43f3b7143baa52d754bc5a5
michalnand <michal.nand@gmail.com>
2018-02-10 13:32:04 +0100

    common

---
+++ src/classification_autoencoder_mnist/main.cpp
@@ -0,0 +1,32 @@
+#include <dataset_landsat.h>
+#include <dataset_mnist.h>
+#include <dataset_mnist_tiny.h>
+
+#include <classification_autoencoder_experiment.h>
+
+
+int main()
+{
+  printf("starting\n");
+
+  CDatasetMnistTiny dataset("/home/michal/dataset/mnist_tiny/training.bin",
+                            "/home/michal/dataset/mnist_tiny/testing.bin");
+
+  std::vector<ClassificationAutoencoderExperiment*> experiments;
+
+  experiments.push_back(new ClassificationAutoencoderExperiment("experiment_parameters/experiment_0.json", &dataset));
+  experiments.push_back(new ClassificationAutoencoderExperiment("experiment_parameters/experiment_1.json", &dataset));
+  experiments.push_back(new ClassificationAutoencoderExperiment("experiment_parameters/experiment_2.json", &dataset));
+  experiments.push_back(new ClassificationAutoencoderExperiment("experiment_parameters/experiment_3.json", &dataset));
+  experiments.push_back(new ClassificationAutoencoderExperiment("experiment_parameters/experiment_4.json", &dataset));
+
+  for (unsigned int i = 0; i < experiments.size(); i++)
+    experiments[i]->run();
+
+  for (unsigned int i = 0; i < experiments.size(); i++)
+    experiments[i]->join();
+
+  printf("program done\n");
+
+  return 0;
+}
