fa7a911050069aa2b4d0bbe232843ca3a0bf7f06
michal <michal.nand@gmail.com>
2018-03-14 11:24:28 +0100

    common

--- src/rl_arcade/main.cpp
+++ src/rl_arcade/main.cpp
@@ -9,64 +9,114 @@
 #include <timer.h>
 #include <getch.h>
 
-
-
-int main()
+void fnn_test(unsigned int idx, unsigned int training_iterations = 300000, unsigned int testing_iterations = 50000)
 {
-  unsigned int iteration = 0;
+  std::string training_progress_log;
+  std::string testing_progress_log;
+  std::string network_result;
 
-  math.srand(time(NULL));
+  training_progress_log = "fnn_progress/training_" + std::to_string(idx) + ".log";
+  testing_progress_log  = "fnn_progress/testing_" + std::to_string(idx) + ".log";
+  network_result        = "fnn_trained_" + std::to_string(idx);
+
+  unsigned int iteration;
 
   Arcade env;
 
-  /*
-  while (1)
-  {
-    int key = getch();
-    unsigned int action = 0;
-
-    if (key == ' ')
-      action = 1;
-
-    env.do_action(action);
-    env.visualisation();
-
-    timer.sleep_ms(100);
-  }
-  */
-
-/*
   RL_HFNN_Agent agent(&env, "fnn");
 
+  iteration = 0;
 
-  for (unsigned int i = 0; i < 200000; i++)
+  for (unsigned int i = 0; i < training_iterations; i++)
   {
     agent.process_learn();
     env.visualisation();
     if ((iteration%100000) == 0)
-      agent.save("fnn_trained");
+      agent.save(network_result);
 
     if ((iteration%1000) == 0)
-      env.log();
+      env.log(training_progress_log);
     iteration++;
   }
 
-  agent.save("fnn_trained");
-*/
+  agent.save(network_result);
 
 
-  RL_FNN_Agent agent(&env, "fnn_trained/supervised");
+  RL_FNN_Agent agent_trained(&env, network_result+"/supervised");
+  iteration = 0;
 
-  for (unsigned int i = 0; i < 40000; i++)
+  for (unsigned int i = 0; i < testing_iterations; i++)
   {
-    agent.process();
+    agent_trained.process();
     env.visualisation();
-    timer.sleep_ms(10);
     if ((iteration%1000) == 0)
-      env.log();
+      env.log(testing_progress_log);
     iteration++;
   }
 
+}
+
+
+void hnn_test(unsigned int idx, unsigned int training_iterations = 300000, unsigned int testing_iterations = 50000)
+{
+  std::string training_progress_log;
+  std::string testing_progress_log;
+  std::string network_result;
+
+  training_progress_log = "hnn_progress/training_" + std::to_string(idx) + ".log";
+  testing_progress_log  = "hnn_progress/testing_" + std::to_string(idx) + ".log";
+  network_result        = "hnn_trained_" + std::to_string(idx);
+
+  unsigned int iteration;
+
+  Arcade env;
+
+  RL_HFNN_Agent agent(&env, "hnn");
+
+  iteration = 0;
+
+  for (unsigned int i = 0; i < training_iterations; i++)
+  {
+    agent.process_learn();
+    env.visualisation();
+    if ((iteration%100000) == 0)
+      agent.save(network_result);
+
+    if ((iteration%1000) == 0)
+      env.log(training_progress_log);
+    iteration++;
+  }
+
+  agent.save(network_result);
+
+
+  RL_FNN_Agent agent_trained(&env, network_result+"/supervised");
+
+  iteration = 0;
+
+  for (unsigned int i = 0; i < testing_iterations; i++)
+  {
+    agent_trained.process();
+    env.visualisation();
+    if ((iteration%1000) == 0)
+      env.log(testing_progress_log);
+    iteration++;
+  }
+
+}
+
+int main()
+{ 
+  math.srand(time(NULL));
+
+  unsigned int training_iterations = 300000;
+  unsigned int testing_iterations  = 50000;
+
+  for (unsigned int i = 0; i < 5; i++)
+    fnn_test(i, training_iterations, testing_iterations);
+
+  for (unsigned int i = 0; i < 5; i++)
+    hnn_test(i, training_iterations, testing_iterations);
 
   printf("program done\n");
 
